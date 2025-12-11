c16b0162ef12745fa4e1e520ead10ef8b530dfdb
Ruzhan Reaz <154721665+ruzhanreaz@users.noreply.github.com>
2025-12-11 21:10:46 +0600

    Delete gomoku directory

--- gomoku/src/main/java/com/example/gomoku/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.gomoku;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
