2cef189acbc55147a11d71529c98b7e6e0e4ceb3
Emam Hassan <151499841+Emam1523@users.noreply.github.com>
2025-12-29 01:22:25 +0600

    Refactor scene setup to use BorderPane layout

--- gomoku/src/main/java/com/example/gomoku/HelloApplication.java
+++ gomoku/src/main/java/com/example/gomoku/HelloApplication.java
@@ -2,7 +2,9 @@ package com.example.gomoku;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +13,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 670);
+        Parent content = fxmlLoader.load();
+
+        BorderPane root = new BorderPane();
+        root.setCenter(content);
+
+        Scene scene = new Scene(root, 820, 670);
         stage.setTitle("Gomoku");
         stage.setScene(scene);
         stage.show();
